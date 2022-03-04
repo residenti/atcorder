bfd6c55cf6a0b61c697ac26d695122f81e039ba9
residenti <n.queue.r@gmail.com>
2022-03-04 21:55:37 +0900

    [others] refactoring max -> max_by

--- others/ls.rb
+++ others/ls.rb
@@ -9,7 +9,7 @@ items = Dir.glob('*').sort
 colum_length = 6.0
 row_length = (items.length / colum_length).ceil
 items_grouped_by_column = items.in_groups_of(row_length, '').map do |column_data|
-  max_length = column_data.max { |a, b| a.length <=> b.length }.length
+  max_length = column_data.max_by(&:length).length
   column_data.map { |item| item.ljust(max_length) }
 end
 items_grouped_by_column.transpose.each { |row| puts row.join(' ') }
