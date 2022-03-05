61b5e451b1cce59c41e5b7f454bd48bf485992f7
residenti <n.queue.r@gmail.com>
2022-03-05 23:24:43 +0900

    [B 増減管理] リファクタリングした

--- B.rb
+++ B.rb
@@ -1,25 +1,7 @@
-input = [10, 9, 10, 3, 100, 100, 90, 80, 10, 30, 10]
-
-input.shift(1)
-arr = input.map.with_index(1) do |x, i|
-  next if input[i].nil?
-  diff = input[i] - x
-  if diff.zero?
-    'stay'
-  elsif diff.negative?
-    "down #{diff.abs}"
-  else
-    "up #{diff}"
-  end
-end
-arr.delete(nil)
-p arr
-
-# refactoring
-values = [10, 9, 10, 3, 100, 100, 90, 80, 10, 30, 10]
-values.shift(1)
-values.each.with_index(1) do |current_value, index|
-  diff = values[index] - current_value
+nums = $stdin.read.split("\n").map(&:to_i)
+nums.shift(1)
+nums.each.with_index(1) do |current_num, index|
+  diff = nums[index] - current_num
   if diff.zero?
     puts 'stay'
   elsif diff.negative?
